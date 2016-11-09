a29d6f92605bc586957aa577e7f6815e485e55fa
ginei11 <carr900@gmail.com>
2016-11-09 11:38:55 -0500

    mejoras

--- src/android/com/ingidear/plugin/webview/WebViewActivity.java
+++ src/android/com/ingidear/plugin/webview/WebViewActivity.java
@@ -1,36 +1,43 @@
 package com.ingidear.webview;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.app.Application;
-import android.content.res.Resources;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class WebViewActivity extends AppCompatActivity {
+import com.ingidear.webview.utilidades.FakeR;
+
+
+public class WebViewActivity extends Activity {
 
     private String TAG = WebViewActivity.class.getSimpleName();
+    private ProgressBar progress;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FakeR fakeR = new FakeR(getApplication().getApplicationContext());
 
-        Application app=cordova.getActivity().getApplication();
-        String package_name = app.getPackageName();
-        Resources resources = app.getResources();
-        int ic = resources.getIdentifier("activity_webview", "layout", package_name);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(fakeR.getId("layout", "activity_webview"));
 
-        setContentView(ic);
+        webView = (WebView) this.findViewById(fakeR.getId("id", "webview"));
 
-        WebView webView = (WebView) this.findViewById(R.id.webview);
+        progress = (ProgressBar) findViewById(fakeR.getId("id", "progressBar"));
+        progress.setVisibility(View.GONE);
 
         String url = "https://www.google.com";
         String nombreApp = "WebViewPlugin";
@@ -48,23 +55,31 @@ public class WebViewActivity extends AppCompatActivity {
                 Log.e(TAG, "Error " + e);
             }
         }
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-
-                WebViewActivity.this.setProgress(progress * 1000);
-            }
-        });
-        webView.loadUrl(url);
+        /**
+         * Se habilita javascript
+         */
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
         try {
-            //getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(color)));
-            getSupportActionBar().setTitle(nombreApp);
+            if (getActionBar()!=null)
+            {
+                getActionBar().setHomeButtonEnabled(true);
+                getActionBar().setDisplayHomeAsUpEnabled(true);
+                getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(color)));
+                getActionBar().setTitle(nombreApp);
+            }
+
         } catch (Exception ex) {
             Log.e(TAG, "" + ex);
         }
+
+        /**
+         * Se lanza el listener de la pagina web
+         */
+        webView.setWebViewClient(new MWebViewClient());
+        //Se carga la url
+        webView.loadUrl(url);
     }
 
     @Override
@@ -86,4 +101,53 @@ public class WebViewActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private class MWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            //url = url.replaceAll("https://www.redyerba.com" , myUrl);
+            logToast("MWebViewClient " + url);
+            view.loadUrl(url);
+            return true;
+        }
+
+        /**
+         * Listener cuando la pagina termina de cargar
+         *
+         * @param view
+         * @param url
+         */
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            progress.setVisibility(View.GONE);
+            WebViewActivity.this.progress.setProgress(100);
+
+            logToast("onPageFinished " + url);
+            super.onPageFinished(view, url);
+        }
+
+        /**
+         * Listener cuando se empieza a cargar la pagina
+         *
+         * @param view
+         * @param url
+         * @param favicon
+         */
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            progress.setVisibility(View.VISIBLE);
+            WebViewActivity.this.progress.setProgress(0);
+
+            logToast("onPageStarted " + url);
+            super.onPageStarted(view, url, favicon);
+        }
+    }
+
+    private void logToast(String mensaje) {
+
+        CharSequence text = mensaje;
+        int duration = Toast.LENGTH_SHORT;
+        Log.d(TAG, mensaje);
+        Toast toast = Toast.makeText(this, text, duration);
+        //toast.show();
+    }
 }
